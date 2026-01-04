a7541d6660fd27475d2f8129219227ad7446955f
Rafa Serrano <rsa00016@red.ujaen.es>
2026-01-04 18:25:26 +0100

    [FEATURE] Estadísticas reubicadas #11

--- estadisticas/models.py
+++ estadisticas/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from torneo.models import Torneo
+from usuario.models import Jugador
 
-# Create your models here.
+class EstadisticasFutbol(models.Model):
+    torneo = models.ForeignKey(Torneo, null=False, blank=False, on_delete=models.CASCADE,  related_name='estadisticas_futbol')
+    jugador = models.ForeignKey(Jugador, null=False, blank=False, on_delete=models.CASCADE, related_name='estadisticas_futbol')
+    goles = models.PositiveIntegerField(null=False, default=0)
+    asistencias = models.PositiveIntegerField(null=False, default=0)
+    goles_contra = models.PositiveIntegerField(null=False, default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['torneo', 'jugador'], name='unique_torneo_jugador_estadisticas_futbol')
+        ]
+
+    def __str__(self):
+        return f'Estadisticas de {self.jugador.user.username} en {self.torneo.nombre}'
+    
+
+class EstadisticasBaloncesto(models.Model):
+    torneo = models.ForeignKey(Torneo, null=False, blank=False, on_delete=models.CASCADE,  related_name='estadisticas_baloncesto')
+    jugador = models.ForeignKey(Jugador, null=False, blank=False, on_delete=models.CASCADE, related_name='estadisticas_baloncesto')
+    puntos = models.PositiveIntegerField(null=False, default=0)
+    rebotes = models.PositiveIntegerField(null=False, default=0)
+    asistencias = models.PositiveIntegerField(null=False, default=0)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['torneo', 'jugador'], name='unique_torneo_jugador_estadisticas_baloncesto')
+        ]
+
+    def __str__(self):
+        return f'Estadisticas de {self.jugador.user.username} en {self.torneo.nombre}'
+
--- torneo/models.py
+++ torneo/models.py
@@ -89,35 +89,5 @@ class Clasificacion(models.Model):
         return f'Clasificacion: {self.equipo.nombre} en {self.torneo.nombre}'
     
 
-class EstadisticasFutbol(models.Model):
-    torneo = models.ForeignKey(Torneo, null=False, blank=False, on_delete=models.CASCADE,  related_name='estadisticas_futbol')
-    jugador = models.ForeignKey(Jugador, null=False, blank=False, on_delete=models.CASCADE, related_name='estadisticas_futbol')
-    goles = models.PositiveIntegerField(null=False, default=0)
-    asistencias = models.PositiveIntegerField(null=False, default=0)
-    goles_contra = models.PositiveIntegerField(null=False, default=0)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['torneo', 'jugador'], name='unique_torneo_jugador_estadisticas_futbol')
-        ]
-
-    def __str__(self):
-        return f'Estadisticas de {self.jugador.user.username} en {self.torneo.nombre}'
-    
-
-class EstadisticasBaloncesto(models.Model):
-    torneo = models.ForeignKey(Torneo, null=False, blank=False, on_delete=models.CASCADE,  related_name='estadisticas_baloncesto')
-    jugador = models.ForeignKey(Jugador, null=False, blank=False, on_delete=models.CASCADE, related_name='estadisticas_baloncesto')
-    puntos = models.PositiveIntegerField(null=False, default=0)
-    rebotes = models.PositiveIntegerField(null=False, default=0)
-    asistencias = models.PositiveIntegerField(null=False, default=0)
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(fields=['torneo', 'jugador'], name='unique_torneo_jugador_estadisticas_baloncesto')
-        ]
-
-    def __str__(self):
-        return f'Estadisticas de {self.jugador.user.username} en {self.torneo.nombre}'
 
 
