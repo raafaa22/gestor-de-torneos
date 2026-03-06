277fbf77a321ecf0368c999ba591eae38c488073
Rafa Serrano <rsa00016@red.ujaen.es>
2026-03-06 13:25:31 +0100

    [FEATURE] Perfil Usuario implementado #41

--- usuario/forms.py
+++ usuario/forms.py
@@ -2,9 +2,9 @@ from django import forms
 from django.utils.translation import gettext as _
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
 
-from usuario.models import Organizador
+from usuario.models import Administrador, Organizador, Jugador
 from equipo.models import Equipo
 
 
@@ -49,6 +49,36 @@ class UserRegisterForm(UserCreationForm):
             user.save()
         
         return user
+
+
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ['email']
+
+    def clean_email(self):
+        email = (self.cleaned_data.get("email") or "").strip().lower()
+        if not email:
+            raise ValidationError(_("El correo es obligatorio."))
+        
+        usuario = User.objects.filter(email__iexact=email) | User.objects.filter(username__iexact=email)
+        if self.instance and self.instance.pk:
+            usuario = usuario.exclude(pk=self.instance.pk)
+
+        if usuario.exists():
+            raise ValidationError(_("Ya existe una cuenta con este correo electrónico."))
+        
+        return email
+    
+
+    def save(self, commit = True):
+        usuario = super().save(commit=False)
+        email = self.cleaned_data.get("email").strip().lower()
+        usuario.username = usuario.email
+        if commit:
+            usuario.save()
+            
+        return usuario
     
 
 class OrganizadorForm(forms.ModelForm):
@@ -71,6 +101,38 @@ class EquipoForm(forms.ModelForm):
         self.fields['deporte'].label = _("Deporte")
 
 
+class AdministradorForm(forms.ModelForm):
+    class Meta:
+        model = Administrador
+        fields = ["nombre"]
+        labels = {
+            "nombre": _("Nombre"),
+        }
+
+class JugadorForm(forms.ModelForm):
+    class Meta:
+        model = Jugador
+        fields = ["dni", "nombre", "apellidos", "es_portero"]
+        labels = {
+            "dni": _("DNI"),
+            "nombre": _("Nombre"),
+            "apellidos": _("Apellidos"),
+            "es_portero": _("Es portero"),
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        if self.instance and self.instance.pk:
+            self.fields["dni"].disabled = True
+
+    def clean_dni(self):
+        if self.instance and self.instance.pk:
+            return self.instance.dni
+
+        dni = (self.cleaned_data.get("dni") or "").strip().upper()
+        return dni
+
 class EmailAuthenticationForm(AuthenticationForm):
     
     def __init__(self, request = ..., *args, **kwargs):
@@ -80,4 +142,3 @@ class EmailAuthenticationForm(AuthenticationForm):
         self.fields['username'].widget.attrs.update({'placeholder': _("Correo Electrónico")})
         self.fields['password'].widget.attrs.update({'placeholder': _("Contraseña")})
         self.fields['password'].widget.attrs.update({'autocomplete': 'current-password'})
-    
