e4b0018d0035f46844f05c2604e33295ca4f1444
Rafa Serrano <rsa00016@red.ujaen.es>
2026-03-06 13:46:54 +0100

    [FEATURE] Traducciones y arreglos finales en vista perfil #41

--- usuario/forms.py
+++ usuario/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.utils.translation import gettext as _
+from django.utils.translation import gettext_lazy as _
 from django.contrib.auth import get_user_model
 from django.core.exceptions import ValidationError
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm, PasswordChangeForm
