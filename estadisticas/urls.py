2125f6f54b52ddcf6e941c9f7e3f60ac1c80b85a
Rafa Serrano <rsa00016@red.ujaen.es>
2026-04-07 17:10:14 +0200

    [FEATURE] Pequeños ajustes en nuevas funciones

--- estadisticas/urls.py
+++ estadisticas/urls.py
@@ -6,6 +6,6 @@ app_name = 'estadisticas'
 
 urlpatterns = [
     path('torneo/<int:torneo_id>/estadisticas/', estadisticas_torneo, name='estadisticas_torneo'),
-    path('torneo/<int:torneo_id>/jugador/<str:jugador_dni>/', jugador_estadisticas_detalle, name='jugador_estadisticas_detalle'),
+    path('torneo/<int:torneo_id>/estadisticas/<str:jugador_dni>/', jugador_estadisticas_detalle, name='jugador_estadisticas_detalle'),
 ]
 
